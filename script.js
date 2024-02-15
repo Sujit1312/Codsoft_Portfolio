d7511777c13744af84eb957905860efc76d7c95d
Sujit Kale <152878394+Sujit1312@users.noreply.github.com>
2024-02-15 15:12:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function sayhello() {
+
+    document.write("Name Sujit Kale<br>");
+    document.write("Email Sujitkale131...@gmail.com<br>");
+    document.write("Number 84217598...<br>");
+    document.write("mesaage Hello Sujit<br>")
+
+}
